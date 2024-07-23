e288aaf45196788a121862ef2010cfbf64cb9602
Feriansyah <devferiansyah@gmail.com>
2024-07-23 18:19:54 +0800

    feat: change progressbar from progress tag to aria progress

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -7,62 +7,71 @@ import VisuallyHidden from '../VisuallyHidden'
 
 const SIZES = {
   small: {
-    '--padding': '0',
-    '--height': '8px',
+    padding: '0',
+    height: '8px',
+    radius: '4px',
   },
   medium: {
-    '--padding': '0',
-    '--height': '12px',
+    padding: '0',
+    height: '12px',
+    radius: '4px',
   },
   large: {
-    '--padding': '4px',
-    '--height': '24px',
+    padding: '4px',
+    height: '24px',
+    radius: '8px',
   },
 }
 
 const ProgressBar = ({ value, size }) => {
-  const stylesSize = SIZES[size]
+  const styles = SIZES[size]
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`)
+  }
+
   return (
-    <div>
-      <VisuallyHidden>
-        <label htmlFor="progressLabel">progress bar</label>
-      </VisuallyHidden>
-      <Progress
-        id="progressLabel"
-        min="0"
-        max="100"
-        value={value}
-        style={stylesSize}
-      />
-    </div>
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        '--padding': styles.padding,
+        '--radius': styles.radius,
+      }}
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <BarWrapper>
+        <Bar
+          style={{
+            '--width': value + '%',
+            '--height': styles.height,
+          }}
+        />
+      </BarWrapper>
+    </Wrapper>
   )
 }
 
-const Progress = styled.progress`
-  height: var(--height);
-  padding: var(--padding);
-  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+const Wrapper = styled.div`
   background-color: ${COLORS.transparentGray15};
-  border: none;
-  border-radius: 4px;
-
-  &::-moz-progress-bar {
-    background-color: ${COLORS.primary};
-    border-radius: ${(p) => (p.value === 100 ? '4px' : '4px 0 0 4px')};
-  }
-
-  /* Chrome */
-  /* &::-webkit-progress-bar {
-    height: var(--height);
-    padding: var(--padding);
-    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-    background-color: ${COLORS.transparentGray15};
-    border-radius: 4px;
-  }
-
-  &::-webkit-progress-value {
-    background-color: ${COLORS.primary};
-  } */
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--radius);
+  padding: var(--padding);
 `
 
+const BarWrapper = styled.div`
+  border-radius: 4px;
+
+  /* Trim off corners when progress bar is near-full. */
+  overflow: hidden;
+`
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+`
 export default ProgressBar
